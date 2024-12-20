a04118e4ca8bd5c804b6671cab96a88acfe59761
Josh <91017208+J-shw@users.noreply.github.com>
2024-12-20 19:57:29 +0200

    UI now loads

--- app/index.tsx
+++ app/index.tsx
@@ -8,11 +8,9 @@ const Stack = createNativeStackNavigator();
 
 export default function App () {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+    </Stack.Navigator>
   );
 };
