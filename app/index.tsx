cbee120fc802c154e5fc3e73864f64a9f74d32a7
Josh <91017208+J-shw@users.noreply.github.com>
2024-12-20 17:55:30 +0200

    Loads data with button and on screen load

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,28 @@
 import { StyleSheet, Text, View } from "react-native";
 import { SystemBars } from "react-native-edge-to-edge";
+import React, { useEffect } from "react"; // Import useEffect
+import { Button } from 'react-native';
+
+async function fetchFact() {
+  const response = await fetch('https://dogapi.dog/api/v2/facts');
+  const data = await response.json();
+  alert('Hello ' + data.data[0].attributes.body);
+}
 
 export default function Page() {
+
+  useEffect(() => { 
+    fetchFact(); 
+  }, []);
 
   return (
     <View style={styles.container}>
       <SystemBars style="auto" />
       <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
+        <Text style={styles.title}>Pawfect Pairing</Text>
+        <Text style={styles.subtitle}>Find your new best friend</Text>
       </View>
+      <Button onPress={() => fetchFact()} title="Fetch a fact!" />
     </View>
   );
 }
