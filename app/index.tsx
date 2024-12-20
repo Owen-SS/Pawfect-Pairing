c962d3ed81adc9fe4a22720c7d7dc157475eb993
Owen Staddon <owen.staddon@icloud.com>
2024-12-20 23:40:20 +0000

    fixes merge conflicts

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,19 @@
+// Page.js
 import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import BreedList from '../components/Cards';
+import DogFact from '../components/DogFact';
+import SwipeableContainer from '../components/SwipeableContainer';
+import { useRouter } from 'expo-router';
 
-export default function App() {
+export default function Page() {
+  const router = useRouter();
+
+  const navigateToMain = () => {
+    router.push('/main');
+  };
+
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}> 
-      <SafeAreaView style={styles.container}>
-        <BreedList />
-      </SafeAreaView>
-    </GestureHandlerRootView>
+    <SwipeableContainer navigateToMain={navigateToMain}>
+      <DogFact />
+    </SwipeableContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f5f5f5',
-  },
-});
