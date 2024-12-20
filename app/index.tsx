9e8b45c023b276c62021c5c41bf908f820eb20c6
Josh <91017208+J-shw@users.noreply.github.com>
2024-12-20 17:39:16 +0200

    Enabled systemBars auto styling
    Changes system bar colours

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,11 @@
 import { StyleSheet, Text, View } from "react-native";
+import { SystemBars } from "react-native-edge-to-edge";
 
 export default function Page() {
+
   return (
     <View style={styles.container}>
+      <SystemBars style="auto" />
       <View style={styles.main}>
         <Text style={styles.title}>Hello World</Text>
         <Text style={styles.subtitle}>This is the first page of your app.</Text>
