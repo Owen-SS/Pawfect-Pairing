5f2c25ea230c4c5a4663dd15b9d3d3f370f9cc41
Owen Staddon <owen.staddon@icloud.com>
2024-12-20 17:43:10 +0000

    Makes react multipage and component based

--- app/index.tsx
+++ app/index.tsx
@@ -1,66 +1,18 @@
-import { StyleSheet, Text, View } from "react-native";
-import { SystemBars } from "react-native-edge-to-edge";
-import React, { useEffect , useState } from "react";
-import { Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-async function fetchDog() {
-  const response = await fetch('https://dogapi.dog/api/v2/facts');
-  const data = await response.json();
-  let fact = data.data;
-  return fact
-}
+import HomeScreen from '../components/HomeScreen'; 
+import ProfileScreen from '../components/ProfileScreen'; 
 
+const Stack = createNativeStackNavigator();
 
-
-export default function Page() {
-  const [dogData, setDogData] = useState({ name: "", description: "" }); // State to store both name and description
-
-  useEffect(() => { 
-    fetchDog().then(data => setDogData(data)); // Update state with the fetched data
-  }, []);
-
+export default function App () {
   return (
-    <View style={styles.container}>
-      <SystemBars style="auto" />
-      <View style={styles.main}>
-        <Text style={styles.title}>Pawfect Pairing</Text>
-        <Text style={styles.subtitle}>Find your new best friend</Text>
-      </View>
-
-      <View style={styles.factBox}> 
-        <Text>{dogData.name}</Text> 
-        <Text>{dogData.description}</Text> 
-      </View>
-
-      {/* <Button onPress={() => fetchFact()} title="Fetch a fact!" /> */}
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-  factBox: {
-    marginTop: 20,
-    padding: 15,
-    borderRadius: 8, 
-    backgroundColor: "whitesmoke", 
-  },
-});
+};
