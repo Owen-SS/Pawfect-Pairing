4d0f991e68939e9808fd5942368532d6d18a2f46
Josh <91017208+J-shw@users.noreply.github.com>
2024-12-21 15:43:16 +0000

    New swipeable cards

--- app/main/index.tsx
+++ app/main/index.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import BreedList from '../../components/Cards';
+import { View, StyleSheet } from 'react-native';
+import SwipeableCards from '../../components/SwipeableCards';
 import { SystemBars } from "react-native-edge-to-edge";
 
 export default function App() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}> 
-    <SystemBars style="auto" />
-      <SafeAreaView style={styles.container}>
-        <BreedList />
-      </SafeAreaView>
-    </GestureHandlerRootView>
+    <View>
+      <SystemBars style="auto" />
+      <SwipeableCards>
+      </SwipeableCards>
+    </View>
   );
 }
 
