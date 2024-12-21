879dddce6ad093f6708e9083c7762af3a1ef12bf
Josh <91017208+J-shw@users.noreply.github.com>
2024-12-21 14:54:01 +0000

    Fixed edge to edge

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,9 @@
 // Page.js
 import React from 'react';
-import DogFact from '../components/DogFact';
 import SwipeableContainer from '../components/SwipeableContainer';
 import { useRouter } from 'expo-router';
+import { SystemBars } from "react-native-edge-to-edge";
+import { View, StyleSheet } from 'react-native';
 
 export default function Page() {
   const router = useRouter();
@@ -12,8 +13,17 @@ export default function Page() {
   };
 
   return (
-    <SwipeableContainer navigateToMain={navigateToMain}>
-      <DogFact />
-    </SwipeableContainer>
+    <View style={styles.fullScreen}>
+      <SystemBars style="auto" />
+      <SwipeableContainer navigateToMain={navigateToMain}>
+      </SwipeableContainer>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  fullScreen: {
+    flex: 1,
+    alignItems: "center",
+  },
+})
