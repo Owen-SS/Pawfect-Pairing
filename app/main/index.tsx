117726b23bebcce29061948f33f5094562fba6a1
Josh <91017208+J-shw@users.noreply.github.com>
2024-12-21 14:53:50 +0000

    Enabled auto system bars

--- app/main/index.tsx
+++ app/main/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import BreedList from '../../components/Cards';
+import { SystemBars } from "react-native-edge-to-edge";
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}> 
+    <SystemBars style="auto" />
       <SafeAreaView style={styles.container}>
         <BreedList />
       </SafeAreaView>
